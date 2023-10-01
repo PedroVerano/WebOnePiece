efa3ff5f16befabc17507c1eaaf595820cbe140a
PedroVerano <pedroverano2009@yahoo.com.br>
2023-10-01 11:24:49 -0300

    Adiciona javascript e interações com botões

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const botoes = document.querySelectorAll(".botao")
+const personagens = document.querySelectorAll('.personagem');
+
+botoes.forEach((botao, index) => {
+    botao.addEventListener  ('click' , () => {
+        const botaoselecionado = document.querySelector('.botao.selecionado');
+        const personagemSelecionado = document.querySelector('.personagem.selecionado');
+        const botaoindex = index;
+
+        botaoselecionado.classList.remove('selecionado');
+        botao.classList.add('selecionado');
+
+        personagemSelecionado.classList.remove('selecionado');
+        personagens[botaoindex].classList.add('selecionado');
+        console.log(botaoindex);
+
+    });
+});
+
+
+
+
+
+    
